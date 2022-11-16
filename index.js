2f7f8246af282294fb160b415dc2d168294a9a8e
rogeraabbccdd <rogeraabbccdd@gmail.com>
2022-11-16 21:55:04 +0800

    Set tempture unit to °C

--- index.js
+++ index.js
@@ -57,7 +57,15 @@ const main = async () => {
   try {
     // Fetch weather data
     const requests = apis.map(async (api) => {
-      const { data: html } = await axios.get(api)
+      const { data: html } = await axios.get(api, {
+        headers: {
+          cookie: 'awx_user=tp:C',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'accept-language': 'zh-TW,zh;q=0.9,ja-JP;q=0.8,ja;q=0.7,en-US;q=0.6,en;q=0.5,ko;q=0.4',
+          accept: 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+        }
+      })
       const $ = cheerio.load(html)
       const data = []
       $('.daily-wrapper').each(function (i) {
